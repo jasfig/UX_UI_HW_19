c45efb9ea3843aeb53dd4e1224efe4283353323a
jasfig <jasmine101410@gmail.com>
2021-04-28 13:34:33 -0500

    gold hover states

--- index.js
+++ index.js
@@ -4,4 +4,46 @@ $(document).ready(function(){
         $('.preloader').fadeOut("slow");
     }
     setTimeout(fade, 1000);
+    });
+
+
+$("h6").hover(function(){
+    $(this).css({
+        color: "#ac7a08"
+    });
+}, function(){
+    $(this).css({
+        color: "#f4f4f4"
+    });
 });
+
+$("p4").hover(function(){
+    $(this).css({
+        color: "#ac7a08"
+    });
+}, function(){
+    $(this).css({
+        color: "#f4f4f4"
+    });
+});
+
+$("p5").hover(function(){
+    $(this).css({
+        color: "#ac7a08"
+    });
+}, function(){
+    $(this).css({
+        color: "#f4f4f4"
+    });
+});
+
+$("p6").hover(function(){
+    $(this).css({
+        color: "#ac7a08"
+    });
+}, function(){
+    $(this).css({
+        color: "#f4f4f4"
+    });
+});
+
